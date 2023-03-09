e48d73233fef8947baf1e06fbd39bee07297084c
Arshad Husain <arshad.husain1@gmail.com>
2023-03-08 18:52:24 -0700

    Remove references to using different trays
    This will now use any paper that is in either papertray

--- biweekly_colour_print.sh
+++ biweekly_colour_print.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-#IMPORTANT
-#media is 'Letter' because it is using the normal paper in the upper tray
-#(media=Letter)
-
-#The lower tray is Legal sized and reserved for the printer cleaning sheets
-#(media=Legal)
+#This script overlays the date and time onto the default ubuntu testpage.
+#Helps with making sure cron job is working
 
 tmpfile=$(mktemp) && echo $(date) | \
     enscript -B -f Courier-Bold16 -o- | \
     ps2pdf - "$tmpfile" && qpdf /usr/share/cups/data/default-testpage.pdf --overlay "$tmpfile" -- datetime_testpage.pdf && \
-    lp -o media=Letter datetime_testpage.pdf
+    lp datetime_testpage.pdf
